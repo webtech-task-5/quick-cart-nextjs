78d8f5d357758b386dc2064fce518fd2b693395f
pc768-86 <moksedur.rahman.sohan@gmail.com>
2023-06-10 10:09:19 +0600

    db connected

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,14 +1,18 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-
+import type { NextApiRequest, NextApiResponse } from "next";
+import dbConnect from "../../libs/dbConnect";
+import Test from "../../models/Test";
 // fake login
-export default (req: NextApiRequest, res: NextApiResponse) => {
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  await dbConnect();
+
   const request = req.body;
   const email = request.email;
   const password = request.password;
+  await Test.create({ name: email });
 
-  if(email === 'johndoe@mail.com' && password === 'ecommerce') {
+  if (email === "johndoe@mail.com" && password === "ecommerce") {
     res.status(200).json({ status: true });
   } else {
     res.status(401).json({ status: false });
   }
-}
+};
